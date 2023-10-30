8b05f5254aaada5f9cce8a836cd2f8a0af61a01d
Zarpoosh <mzar67839@gmail.com>
2023-10-30 10:53:54 +0330

    change names

--- js/main.js
+++ js/main.js
@@ -41,28 +41,28 @@ const containerProduct = $.querySelector('.overflow');
 console.log(containerProduct);
 //?============= dataProducts =============\\
 const  dataProducts = [
-    {id: 0, imgSrc: './images/product10.png', productName: 'هندزفری',category : "shoes" ,price: '30,0', star: 3 , like:true , off : false , bascket : false},
-    {id: 1, imgSrc: './images/product12.png', productName: 'Clothes 2',category : "shoes" ,price: '25,0', star: 3 , like:true , off : false , bascket : false},
-    {id: 2, imgSrc: './images/product3.png', productName: 'Clothes 3',category : "plant" ,price: '35,0', star: 3 , like:true , off : false , bascket : false},
-    {id: 3, imgSrc: './images/product6.png', productName: 'Clothes 4',category : "shoes" ,price: '22,0', star: 3 , like:true , off : false , bascket : false},
-    {id: 4, imgSrc: './images/product3.png', productName: 'Clothes 5',category : "shoes" ,price: '29,0', star: 3 , like:true , off : false , bascket : false},
-    {id: 5, imgSrc: './images/product3.png', productName: 'Clothes 6',category : "shoes" ,price: '24,0', star: 3 , like:true , off : false , bascket : false},
-    {id: 6, imgSrc: './images/product3.png', productName: 'تیشرت',category : "plant" ,price: '32,0', star: 3 , like:true , off : false , bascket : false},
-    {id: 7, imgSrc: './images/product3.png', productName: 'Clothes 8',category : "shoes" ,price: '40,0', star: 3 , like:true , off : false , bascket : false},
-    {id: 8, imgSrc: './images/product3.png', productName: 'Clothes 9',category : "shoes" ,price: '40,0', star: 3 , like:true , off : false , bascket : false},
-    {id: 9, imgSrc: './images/product3.png', productName: 'Clothes 10', price: '40,0',  star: 3 , category : "shoes" , like:true , off : false , bascket : false},
-    {id: 10, imgSrc: './images/product6.png', productName: 'Clothes 11', price: '40,0', star: 3 , category : "shoes" , like:true , off : false , bascket : false},
-    {id: 11, imgSrc: './images/product6.png', productName: 'Clothes 12', price: '40,0', star: 3 , category : "shoes" , like:true , off : false , bascket : false},
-    {id: 12, imgSrc: './images/product6.png', productName: 'کفش', price: '40,0', star: 3 , category : "plant" , like:true , off : false , bascket : false},
-    {id: 13, imgSrc: './images/product6.png', productName: 'Clothes 14', price: '40,0', star: 3 , category : "shoes" , like:true , off : false , bascket : false},
-    {id: 14, imgSrc: './images/product6.png', productName: 'Clothes 15', price: '40,0', star: 3 , category : "shoes" , like:true , off : false , bascket : false},
-    {id: 15, imgSrc: './images/product6.png', productName: 'Clothes 16', price: '40,0', star: 3 , category : "shoes" , like:true , off : false , bascket : false},
-    {id: 16, imgSrc: './images/product6.png', productName: 'Clothes 17', price: '40,0', star: 3 , category : "shoes" , like:true , off : false , bascket : false},
-    {id: 17, imgSrc: './images/product6.png', productName: 'Clothes 18', price: '40,0', star: 3 , category : "shoes" , like:true , off : false , bascket : false},
-    ]
+  {id: 1, imgSrc: './images/product12.png', productName: 'Clothes 2',category : "زنانه" ,price: '25,0', star: 3 , like:true , off : false , bascket : false},
+  {id: 2, imgSrc: './images/P6.jpg', productName: 'Clothes 3',category : "زنانه" ,price: '35,0', star: 3 , like:true , off : false , bascket : false},
+  {id: 3, imgSrc: './images/P8.jpg', productName: 'Clothes 4',category : "زنانه" ,price: '22,0', star: 3 , like:true , off : false , bascket : false},
+  {id: 4, imgSrc: './images/product1.png', productName: 'Clothes 5',category : "shoes" ,price: '29,0', star: 3 , like:true , off : false , bascket : false},
+  {id: 0, imgSrc: './images/product2.png', productName: 'هندزفری',category : "زنانه" ,price: '30,0', star: 3 , like:true , off : false , bascket : false},
+  {id: 5, imgSrc: './images/product3.png', productName: 'Clothes 6',category : "shoes" ,price: '24,0', star: 3 , like:true , off : false , bascket : false},
+  {id: 6, imgSrc: './images/product4.png', productName: 'تیشرت',category : "plant" ,price: '32,0', star: 3 , like:true , off : false , bascket : false},
+  {id: 7, imgSrc: './images/product5.png', productName: 'Clothes 8',category : "shoes" ,price: '40,0', star: 3 , like:true , off : false , bascket : false},
+  {id: 8, imgSrc: './images/product6.png', productName: 'Clothes 9',category : "shoes" ,price: '40,0', star: 3 , like:true , off : false , bascket : false},
+  {id: 9, imgSrc: './images/product7.png', productName: 'Clothes 10', price: '40,0',  star: 3 , category : "shoes" , like:true , off : false , bascket : false},
+  {id: 10, imgSrc: './images/product8.png', productName: 'Clothes 11', price: '40,0', star: 3 , category : "زنانه" , like:true , off : false , bascket : false},
+  {id: 11, imgSrc: './images/product9.png', productName: 'Clothes 12', price: '40,0', star: 3 , category : "shoes" , like:true , off : false , bascket : false},
+  {id: 12, imgSrc: './images/product10.png', productName: 'کفش', price: '40,0', star: 3 , category : "زنانه" , like:true , off : false , bascket : false},
+  {id: 13, imgSrc: './images/produc11.png', productName: 'Clothes 14', price: '40,0', star: 3 , category : "shoes" , like:true , off : false , bascket : false},
+  {id: 14, imgSrc: './images/product12.png', productName: 'Clothes 15', price: '40,0', star: 3 , category : "shoes" , like:true , off : false , bascket : false},
+  {id: 15, imgSrc: './images/product13.png', productName: 'Clothes 16', price: '40,0', star: 3 , category : "shoes" , like:true , off : false , bascket : false},
+  {id: 16, imgSrc: './images/product14.png', productName: 'Clothes 17', price: '40,0', star: 3 , category : "shoes" , like:true , off : false , bascket : false},
+  {id: 17, imgSrc: './images/product15.png', productName: 'Clothes 18', price: '40,0', star: 3 , category : "shoes" , like:true , off : false , bascket : false},
+  ]
     // ==========> filter data Products <========== \\
     let dataPlanet = dataProducts.filter((item) => {
-        if(item.category === "plant"){
+        if(item.category === "زنانه"){
           console.log(item);
           return item
         }
@@ -114,8 +114,13 @@ const  dataProducts = [
          `)
     }
 
-// ////////////////////  new js  ///////////////////////////
 
+
+
+
+
+
+// ////////////////////  new js  ///////////////////////////
 window.addEventListener("resize", function () {
   if (window.innerWidth < 575) {
     let form = $.getElementById("search-form");
