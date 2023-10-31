84c1f1bf3ea3eaaf760d8a7b24fe430ff4c86f9a
mohmmadView <mamad.rahgoshahi@gmail.com>
2023-10-31 04:33:47 +0330

    func in source

--- js/main.js
+++ js/main.js
@@ -41,81 +41,29 @@ const containerProduct = $.querySelector('.overflow');
 console.log(containerProduct);
 //?============= dataProducts =============\\
 const  dataProducts = [
-  {id: 1, imgSrc: './images/product12.png', productName: 'تیشرت مشکی',category : "زنانه" ,price: '25,0', star: 3 , like:true , off : false , bascket : false},
-  {id: 2, imgSrc: './images/P6.jpg', productName: 'لباس محلی',category : "زنانه" ,price: '35,0', star: 3 , like:true , off : false , bascket : false},
-  {id: 3, imgSrc: './images/P8.jpg', productName: 'دامن مجلسی',category : "زنانه" ,price: '22,0', star: 3 , like:true , off : false , bascket : false},
-  {id: 4, imgSrc: './images/product1.png', productName: 'Clothes 5',category : "shoes" ,price: '29,0', star: 3 , like:true , off : false , bascket : false},
-  {id: 0, imgSrc: './images/product2.png', productName: 'مانتو پاییزی',category : "زنانه" ,price: '30,0', star: 3 , like:true , off : false , bascket : false},
-  {id: 5, imgSrc: './images/product3.png', productName: 'Clothes 6',category : "shoes" ,price: '24,0', star: 3 , like:true , off : false , bascket : false},
-  {id: 6, imgSrc: './images/product4.png', productName: 'تیشرت',category : "plant" ,price: '32,0', star: 3 , like:true , off : false , bascket : false},
-  {id: 7, imgSrc: './images/product5.png', productName: 'Clothes 8',category : "shoes" ,price: '40,0', star: 3 , like:true , off : false , bascket : false},
-  {id: 8, imgSrc: './images/product6.png', productName: 'Clothes 9',category : "shoes" ,price: '40,0', star: 3 , like:true , off : false , bascket : false},
-  {id: 9, imgSrc: './images/product7.png', productName: 'Clothes 10', price: '40,0',  star: 3 , category : "shoes" , like:true , off : false , bascket : false},
-  {id: 10, imgSrc: './images/product8.png', productName: 'کیف مجلسی', price: '40,0', star: 3 , category : "زنانه" , like:true , off : false , bascket : false},
-  {id: 11, imgSrc: './images/product9.png', productName: 'Clothes 12', price: '40,0', star: 3 , category : "shoes" , like:true , off : false , bascket : false},
+  {id: 1,  imgSrc: './images/product12.png', productName: 'تیشرت مشکی'  ,category : "زنانه" ,price: '25,0', star: 3 , like:true , off : false , bascket : false},
+  {id: 2,  imgSrc: './images/P6.jpg',        productName: 'لباس محلی'   ,category : "زنانه" ,price: '35,0', star: 3 , like:true , off : false , bascket : false},
+  {id: 3,  imgSrc: './images/P8.jpg',        productName: 'دامن مجلسی'  ,category : "زنانه" ,price: '22,0', star: 3 , like:true , off : false , bascket : false},
+  {id: 4,  imgSrc: './images/product1.png',  productName: 'Clothes 5' ,category : "shoes" ,price: '29,0', star: 3 , like:true , off : false , bascket : false},
+  {id: 0,  imgSrc: './images/product2.png',  productName: 'مانتو پاییزی'   ,category : "زنانه" ,price: '30,0', star: 3 , like:true , off : false , bascket : false},
+  {id: 5,  imgSrc: './images/product3.png',  productName: 'Clothes 6'  ,category : "shoes" ,price: '24,0', star: 3 , like:true , off : false , bascket : false},
+  {id: 6,  imgSrc: './images/product4.png',  productName: 'تیشرت',      category : "plant" ,price: '32,0', star: 3 , like:true , off : false , bascket : false},
+  {id: 7,  imgSrc: './images/product5.png',  productName: 'Clothes 8',category : "shoes" ,price: '40,0', star: 3 , like:true , off : false , bascket : false},
+  {id: 8,  imgSrc: './images/product6.png',  productName: 'Clothes 9',category : "shoes" ,price: '40,0', star: 3 , like:true , off : false , bascket : false},
+  {id: 9,  imgSrc: './images/product7.png',  productName: 'Clothes 10', price: '40,0',  star: 3 , category : "shoes" , like:true , off : false , bascket : false},
+  {id: 10, imgSrc: './images/product8.png',  productName: 'کیف مجلسی', price: '40,0', star: 3 , category : "زنانه" , like:true , off : false , bascket : false},
+  {id: 11, imgSrc: './images/product9.png',  productName: 'Clothes 12', price: '40,0', star: 3 , category : "shoes" , like:true , off : false , bascket : false},
   {id: 12, imgSrc: './images/product10.png', productName: 'شلوار لی ', price: '40,0', star: 3 , category : "زنانه" , like:true , off : false , bascket : false},
-  {id: 13, imgSrc: './images/produc11.png', productName: 'Clothes 14', price: '40,0', star: 3 , category : "shoes" , like:true , off : false , bascket : false},
+  {id: 13, imgSrc: './images/product11.png', productName: 'Clothes 14', price: '40,0', star: 3 , category : "shoes" , like:true , off : false , bascket : false},
   {id: 14, imgSrc: './images/product12.png', productName: 'Clothes 15', price: '40,0', star: 3 , category : "shoes" , like:true , off : false , bascket : false},
   {id: 15, imgSrc: './images/product13.png', productName: 'Clothes 16', price: '40,0', star: 3 , category : "shoes" , like:true , off : false , bascket : false},
   {id: 16, imgSrc: './images/product14.png', productName: 'Clothes 17', price: '40,0', star: 3 , category : "shoes" , like:true , off : false , bascket : false},
   {id: 17, imgSrc: './images/product15.png', productName: 'Clothes 18', price: '40,0', star: 3 , category : "shoes" , like:true , off : false , bascket : false},
   ]
-    // ==========> filter data Products <========== \\
-    let dataPlanet = dataProducts.filter((item) => {
-        if(item.category === "زنانه"){
-          console.log(item);
-          return item
-        }
-      })
-      // ===========  view data products  <========== \\
-    for(let i = 0; i < dataPlanet.length; i++) {
-      console.log(i, dataPlanet[i]);
-      let product = dataPlanet[i];
-     
-      console.log(product.id);
-      console.log(containerProduct);
-      // Todo  insertAdjacentHTML  replace code Html  \\
-      containerProduct.insertAdjacentHTML('afterbegin',`
-         
-   <div class="product  col-lg-3 col-md-4 col-sm-6 col-xs-12">
-                <div class="shoping-card">
-                  <div class="img-sec">
-                      <img src=${product.imgSrc} alt="">
-                      <span class="hot-offer ">پیشنهاد ویژه</span>
-                      
-                      <div class="stars">
-                          <i class="fa-solid fa-star"></i>
-                          <i class="fa-solid fa-star"></i>
-                          <i class="fa-solid fa-star"></i>
-                          <i class="fa-solid fa-star"></i>
-                      </div>
-                  </div>
-                  <h2>${product.productName}</h2>
-                  <h5>${product.category}</h5>
-                  <div class="buttons">
-                      <div class="right">
-                          <span class="price">${product.price}</span>
-      
-                      </div>
-                      <div class="left">
-                          <div class="extend-btn">
-                          <a class="b-text" href="">علاقه</a>
-                          <a class="b-icon" href=""><i class="fa-solid fa-heart"></i></a>
-                      </div>
-                      <div class="extend-btn">
-                          <a class="b-text" href="">خرید</a>
-                          <a class="b-icon" href=""><i class="fa-solid fa-cart-shopping"></i></a>
-                       </div>
-                    </div>
-                  </div>
-                 </div>
-              </div>
-          
-         `)
-    }
-
-
-
+  var dataFilter;
+  filteredProducts(dataProducts , "زنانه");
+  GeneratesProducts(dataFilter);
+   
 
 
 
@@ -163,6 +111,75 @@ function searchProducts() {
     }
   }
 }
+function handlerDataProducts(category){
 
+}
+/**
+    * Filters the given data based on the specified category.
+    *
+    * @param {Array} data - The data to be filtered.
+    * @param {string} filterCategory - The category to filter the data by.
+    * @return {Array} - The filtered data.
+    */
+   function filteredProducts(data , filterCategory){
+         dataFilter = data.filter((item) => {
+        if(item.category === filterCategory){
+          console.log(item);
+          return item
+        }
+       
+      })
+      console.log(dataFilter);
+       return (dataFilter)
+   } 
+  
+      /**
+       * Generates products based on the provided data filter
+       *is displayed on the screen
+       * @param {Array} dataFilter - The data filter to generate products from.
+       */
+      function GeneratesProducts(dataFilter){
+    for(let i = 0; i < dataFilter.length; i++) {
+      console.log(i, dataFilter[i]);
+      let product = dataFilter[i];
+     
+      console.log(product.id);
+      console.log(containerProduct);
+      // Todo  insertAdjacentHTML  replace code Html  \\
+      containerProduct.insertAdjacentHTML('afterbegin',`
+   <div class="product  col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                <div class="shoping-card">
+                  <div class="img-sec">
+                      <img src=${product.imgSrc} alt="">
+                      <span class="hot-offer ">پیشنهاد ویژه</span>
+                      <div class="stars">
+                          <i class="fa-solid fa-star"></i>
+                          <i class="fa-solid fa-star"></i>
+                          <i class="fa-solid fa-star"></i>
+                          <i class="fa-solid fa-star"></i>
+                      </div>
+                  </div>
+                  <h2>${product.productName}</h2>
+                  <h5>${product.category}</h5>
+                  <div class="buttons">
+                      <div class="right">
+                          <span class="price">${product.price}</span>
+                      </div>
+                      <div class="left">
+                          <div class="extend-btn">
+                          <a class="b-text" href="">علاقه</a>
+                          <a class="b-icon" href=""><i class="fa-solid fa-heart"></i></a>
+                      </div>
+                      <div class="extend-btn">
+                          <a class="b-text" href="">خرید</a>
+                          <a class="b-icon" href=""><i class="fa-solid fa-cart-shopping"></i></a>
+                       </div>
+                    </div>
+                  </div>
+                 </div>
+              </div>
+          
+         `)
+    }
 
-// hello
+  }
--- jscode.js
+++ jscode.js
@@ -1,137 +1,137 @@
-function handlerDataProducts(category){
+// function handlerDataProducts(category){
 
   
-filteredProducts.forEach(product =>{
+// filteredProducts.forEach(product =>{
 
-  // ==========> filter data Products <========== \\
-  let dataPlanet = dataProducts.filter((item) => {
-      if(item.category =category){
-        console.log(item);
-        return item
-      }
-    })
-    // ===========  view data products  <========== \\
+//   // ==========> filter data Products <========== \\
+//   let dataPlanet = dataProducts.filter((item) => {
+//       if(item.category =category){
+//         console.log(item);
+//         return item
+//       }
+//     })
+//     // ===========  view data products  <========== \\
   
-  for(let i = 0; i < dataPlanet.length; i++) {
-    console.log(i, dataPlanet[i]);
-    let product = dataPlanet[i];
+//   for(let i = 0; i < dataPlanet.length; i++) {
+//     console.log(i, dataPlanet[i]);
+//     let product = dataPlanet[i];
    
-    console.log(product.id);
-    console.log(containerProduct);
-    // Todo  insertAdjacentHTML  replace code Html  \\
-    containerProduct.insertAdjacentHTML('afterbegin',`
+//     console.log(product.id);
+//     console.log(containerProduct);
+//     // Todo  insertAdjacentHTML  replace code Html  \\
+//     containerProduct.insertAdjacentHTML('afterbegin',`
        
-  <div class="product  col-lg-3 col-md-4 col-sm-6 col-xs-12">
-              <div class="shoping-card">
-                <div class="img-sec">
-                    <img src=${product.imgSrc} alt="">
-                    <span class="hot-offer ">پیشنهاد ویژه</span>
+//   <div class="product  col-lg-3 col-md-4 col-sm-6 col-xs-12">
+//               <div class="shoping-card">
+//                 <div class="img-sec">
+//                     <img src=${product.imgSrc} alt="">
+//                     <span class="hot-offer ">پیشنهاد ویژه</span>
                     
-                    <div class="stars">
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                    </div>
-                </div>
-                <h2>${product.productName}</h2>
-                <h2>${product.category}</h2>
-                <div class="buttons">
-                    <div class="right">
-                        <span class="price">${product.price}</span>
+//                     <div class="stars">
+//                         <i class="fa-solid fa-star"></i>
+//                         <i class="fa-solid fa-star"></i>
+//                         <i class="fa-solid fa-star"></i>
+//                         <i class="fa-solid fa-star"></i>
+//                     </div>
+//                 </div>
+//                 <h2>${product.productName}</h2>
+//                 <h2>${product.category}</h2>
+//                 <div class="buttons">
+//                     <div class="right">
+//                         <span class="price">${product.price}</span>
     
-                    </div>
-                    <div class="left">
-                        <div class="extend-btn">
-                        <a class="b-text" href="">علاقه</a>
-                        <a class="b-icon" href=""><i class="fa-solid fa-heart"></i></a>
-                    </div>
-                    <div class="extend-btn">
-                        <a class="b-text" href="">خرید</a>
-                        <a class="b-icon" href=""><i class="fa-solid fa-cart-shopping"></i></a>
-                     </div>
-                  </div>
-                </div>
-               </div>
-            </div>
+//                     </div>
+//                     <div class="left">
+//                         <div class="extend-btn">
+//                         <a class="b-text" href="">علاقه</a>
+//                         <a class="b-icon" href=""><i class="fa-solid fa-heart"></i></a>
+//                     </div>
+//                     <div class="extend-btn">
+//                         <a class="b-text" href="">خرید</a>
+//                         <a class="b-icon" href=""><i class="fa-solid fa-cart-shopping"></i></a>
+//                      </div>
+//                   </div>
+//                 </div>
+//                </div>
+//             </div>
         
-       `)
-  }
-})
+//        `)
+//   }
+// })
 
-}  
+// }  
 
-//////////////////////////////////////////////////////////////////////////////////
+// //////////////////////////////////////////////////////////////////////////////////
 
 
-function handlerDataProducts(category) {
-  // Clear the container
-  containerProduct.innerHTML = '';
+// function handlerDataProducts(category) {
+//   // Clear the container
+//   containerProduct.innerHTML = '';
 
-  // Filter products based on the category
-  let filteredProducts = dataProducts.filter(item => item.category === category);
+//   // Filter products based on the category
+//   let filteredProducts = dataProducts.filter(item => item.category === category);
 
-  // Group products by category
-  let groupedProducts = {};
-  filteredProducts.forEach(product => {
-    if (groupedProducts.hasOwnProperty(product.category)) {
-      groupedProducts[product.category].push(product);
-    } else {
-      groupedProducts[product.category] = [product];
-    }
-  });
+//   // Group products by category
+//   let groupedProducts = {};
+//   filteredProducts.forEach(product => {
+//     if (groupedProducts.hasOwnProperty(product.category)) {
+//       groupedProducts[product.category].push(product);
+//     } else {
+//       groupedProducts[product.category] = [product];
+//     }
+//   });
 
-  // Display products by category
-  for (let categoryName in groupedProducts) {
-    let productList = groupedProducts[categoryName];
+//   // Display products by category
+//   for (let categoryName in groupedProducts) {
+//     let productList = groupedProducts[categoryName];
 
-    // Create a new category list
-    let categoryList = document.createElement('ul');
-    categoryList.className = 'category-list';
-    categoryList.innerHTML = `<h2>${categoryName}</h2>`;
+//     // Create a new category list
+//     let categoryList = document.createElement('ul');
+//     categoryList.className = 'category-list';
+//     categoryList.innerHTML = `<h2>${categoryName}</h2>`;
 
-    // Add products to the category list
-    productList.forEach(product => {
-      let listItem = document.createElement('li');
-      listItem.innerHTML = `
-        <div class="product col-lg-3 col-md-4 col-sm-6 col-xs-12">
-          <div class="shoping-card">
-            <div class="img-sec">
-              <img src=${product.imgSrc} alt="">
-              <span class="hot-offer">پیشنهاد ویژه</span>
-              <div class="stars">
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-              </div>
-            </div>
-            <h2>${product.productName}</h2>
-            <h2>${product.category}</h2>
-            <div class="buttons">
-              <div class="right">
-                <span class="price">${product.price}</span>
-              </div>
-              <div class="left">
-                <div class="extend-btn">
-                  <a class="b-text" href="">علاقه</a>
-                  <a class="b-icon" href=""><i class="fa-solid fa-heart"></i></a>
-                </div>
-                <div class="extend-btn">
-                  <a class="b-text" href="">خرید</a>
-                  <a class="b-icon" href=""><i class="fa-solid fa-cart-shopping"></i></a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      `;
+//     // Add products to the category list
+//     productList.forEach(product => {
+//       let listItem = document.createElement('li');
+//       listItem.innerHTML = `
+//         <div class="product col-lg-3 col-md-4 col-sm-6 col-xs-12">
+//           <div class="shoping-card">
+//             <div class="img-sec">
+//               <img src=${product.imgSrc} alt="">
+//               <span class="hot-offer">پیشنهاد ویژه</span>
+//               <div class="stars">
+//                 <i class="fa-solid fa-star"></i>
+//                 <i class="fa-solid fa-star"></i>
+//                 <i class="fa-solid fa-star"></i>
+//                 <i class="fa-solid fa-star"></i>
+//               </div>
+//             </div>
+//             <h2>${product.productName}</h2>
+//             <h2>${product.category}</h2>
+//             <div class="buttons">
+//               <div class="right">
+//                 <span class="price">${product.price}</span>
+//               </div>
+//               <div class="left">
+//                 <div class="extend-btn">
+//                   <a class="b-text" href="">علاقه</a>
+//                   <a class="b-icon" href=""><i class="fa-solid fa-heart"></i></a>
+//                 </div>
+//                 <div class="extend-btn">
+//                   <a class="b-text" href="">خرید</a>
+//                   <a class="b-icon" href=""><i class="fa-solid fa-cart-shopping"></i></a>
+//                 </div>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       `;
 
-      // Add the product to the category list
-      categoryList.appendChild(listItem);
-    });
+//       // Add the product to the category list
+//       categoryList.appendChild(listItem);
+//     });
 
-    // Add the category list to the container
-    containerProduct.appendChild(categoryList);
-  }
-}
+//     // Add the category list to the container
+//     containerProduct.appendChild(categoryList);
+//   }
+// }
